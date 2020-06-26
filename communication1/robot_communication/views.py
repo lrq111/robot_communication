ff8e69cc84827752096af0c06066a168baee811a
snileyk <smileyk11@foxmail.com>
2020-06-26 22:56:31 +0800

    ！add the diagnosis inference methods

--- communication1/robot_communication/views.py
+++ communication1/robot_communication/views.py
@@ -3,7 +3,9 @@ import json
 from django.shortcuts import redirect
 import json
 import os
-from robot_communication.models import Sys_user,Communication
+from robot_communication.models import Sys_user, Communication
+from util.nlp_utility import NLPUtility
+from util.csv_utility import CsvUtility
 from django.contrib.auth.models import User
 from django.contrib import auth
 # import tensorflow as tf
@@ -73,25 +75,80 @@ def regist(request):
                     logininfo = 'wrong'
                     return render(request, 'regist.html', {'logininfo': logininfo})
 
+def _get_dict(data, reverse=True):
+    data_dict = {}
+    if reverse:
+        a = 1
+        b = 0
+    else:
+        a = 0
+        b = 1
+    for i in data:
+        sp = i.split("\t")
+        if sp[a] in data_dict:
+            data_dict[sp[a]].add(sp[b])
+        else:
+            data_dict[sp[a]] = set([sp[b]])
+    return data_dict
+
+def get_first_diag(chief_complaint, disease_dict, syptom_dict):
+    nu_dis = NLPUtility(word_pattern_file=rootPath + "/data/load_dict_jieba.csv")
+    nu_dis.load_jieba_model()
+    diag_count = nu_dis.key_word_extract(chief_complaint, {**disease_dict, **syptom_dict})
+    # print(diag_count)
+    diag_list = [i[0] for i in sorted(diag_count.items(), key=lambda x: x[1], reverse=True)]
+
+    robot_response = "最有可能是：\"" + diag_list[0] + "\""
+    if len(diag_list) > 1:
+        robot_response += "，还有可能是："
+        for diag_i in range(1, len(diag_list)):
+            robot_response += "\"" + diag_list[diag_i] + "\""
+            # if diag_i < len(diag_list) - 1:
+            #     robot_response += "、"
+            if diag_i < len(diag_list) - 1:
+                robot_response += "、"
+    return robot_response, diag_list
+
+# 返回第diag_index个疾病的检验检查项
+def get_labtest(diag_list, diag_index, phy_dict, labtest_dict):
+    diag_now = diag_list[diag_index]
+    labtest_result = []
+    if diag_now in phy_dict:
+        for phy_i in phy_dict[diag_now]:
+            labtest_result.append(labtest_dict[phy_i])
+    return labtest_result
+
+# 根据用户的检验检查结果，与第diag_index个疾病的生理指标相匹配，满足条件返回True
+def match_phy_condition(phy_result, diag_list, diag_index, phy_dict):
+    nu_phy=NLPUtility(word_pattern_file=rootPath + "/data/生理指标实例.csv")
+    nu_phy.load_jieba_model()
+    phy_sub, phy_cond = nu_phy.condition_extract(phy_result, CsvUtility.read_norm_array_csv(rootPath + "/data/生理指标实例.csv"))
+    for sub_i in range(len(phy_sub)):
+        if phy_sub[sub_i]+"##"+phy_cond[sub_i] in phy_dict[diag_list[diag_index]]:
+            return True
+    return False
 
 def robot_response(request):
-    dt=datetime.datetime.now()
+    dt = datetime.datetime.now()
     print(dt)
+
+    print("......加载模型中......")
+    disease_dict = _get_dict(CsvUtility.read_norm_array_csv(rootPath + '/data/病史关系.csv'))
+    syptom_dict = _get_dict(CsvUtility.read_norm_array_csv(rootPath + '/data/疾病症状关系.csv'))
+    CsvUtility.write_word_dict(rootPath + '/data/load_dict_jieba.csv', list(disease_dict.keys()) + list(syptom_dict.keys()))
+    # print(syptom_dict)
+    phy_dict = _get_dict(CsvUtility.read_norm_array_csv(rootPath + '/data/疾病-指标条件关系.csv'), reverse=False)
+    labtest_dict = _get_dict(CsvUtility.read_norm_array_csv(rootPath + '/data/生理指标-检验检查关系.csv'))
+    print("......加载完成......")
+
     if request.method=='GET':
         # print("get了")
         my_text=request.GET.get("my_text")    #此处为算法输入
         print(my_text)
-        new_content_my=Communication(com_type="human",com_content=my_text)
+        new_content_my = Communication(com_type="human",com_content=my_text)
         new_content_my.save()
-        #插入后端算法，我这里使用了一个candidate列表来代替
-        candidate_list=['感冒','发烧','腮腺炎']
-        import random
 
-
-        # dt_time=dt.strftime('%Y-%m-%d %H:%M:%S')
-        # print(dt_time)
-        robot_response=random.choice(candidate_list)   #此处更换为后端算法的输出
-
+        robot_response, diag_list = get_first_diag(my_text, disease_dict, syptom_dict)
         new_content_robot = Communication(com_type="robot", com_content=robot_response)
         new_content_robot.save()
         # from django.core import serializers
@@ -118,6 +175,7 @@ def delete_all(request):
         # for i in communication_list:
         #     print(i.com_content)
         return render(request, 'index.html', {'user': user, 'sys_user': sys_user, 'communication_list': communication_list})
+
 # def ajax_demo1(request):
 #     return render(request, "ajax_demo1.html")
 #
---
+++ communication1/util/nlp_utility.py
@@ -0,0 +1,43 @@
+# Set your own model path
+
+import sys
+import os
+import re
+from pyltp import Segmentor, Postagger, Parser, NamedEntityRecognizer, SentenceSplitter
+import jieba
+
+class NLPUtility(object):
+	def __init__(self, word_pattern_file="H:\\new_github_workspace\\Syptom_Knowledge_Graph_309\\test_result.csv"):
+		self.word_pattern_file = word_pattern_file
+
+	def load_jieba_model(self):
+		print("正在加载jieba模型... ...")
+		jieba.load_userdict(self.word_pattern_file)
+		print("加载模型完毕。")
+
+	def key_word_extract(self, sentence, data_dict):
+		value_count = {}
+		seg_list = jieba.cut(sentence)
+		# print(seg_list)
+		for word in seg_list:
+			if word in data_dict:
+				vals = data_dict[word]
+				for val in vals:
+					if val in value_count:
+						value_count[val] += 1
+					else:
+						value_count[val] = 1
+		return value_count
+
+	def condition_extract(self, text, data_dict):
+		sentences = re.split(r'(\.|\!|\?|。|！|？|\.{6})', text)
+		sub = []
+		cond = []
+		for sentence in sentences:
+			seg_list = jieba.cut(sentence)
+			for w_i, word in enumerate(seg_list):
+				if word in data_dict and w_i < len(seg_list) - 1:
+					sub.append(word)
+					cond.append(seg_list[w_i+1])
+		return sub, cond
+
